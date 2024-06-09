4c0c5c9c56f1e99742232e2ca0c525f6a9af3720
vukd1 <vuk.dojcinovic@gmail.com>
2024-06-10 00:30:50 +0200

    1. Remade the login interface 2. The login interface now logs the user in once enter is pressed on any element 3. Changed the main hub logic so that now open storefronts properly close instead of hiding

--- src/main/java/com/example/cs202pz/view/LoginView.java
+++ src/main/java/com/example/cs202pz/view/LoginView.java
@@ -1,29 +1,47 @@
 package com.example.cs202pz.view;
 
 import com.example.cs202pz.controller.LoginController;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class LoginView extends Stage {
 
     private static LoginView loginView_instance = null;
-    private BorderPane bp = new BorderPane();
+    private Label usernameLabel = new Label("Username:");
+    private Label passwordLabel = new Label("Password:");
     private TextField username = new TextField();
     private PasswordField password = new PasswordField();
     private Button loginButton = new Button("Login");
-    private VBox vBox1 = new VBox(username, password);
-    private Label topLabel = new Label("Welcome, please log in");
+    private Label welcomeLabel1 = new Label("Welcome");
+    private Label welcomeLabel2 = new Label("Please fill in your credentials");
+    private Label helpButton = new Label("Help");
+    private VBox vBox1 = new VBox(welcomeLabel1, welcomeLabel2);
+    private HBox hBox1 = new HBox(usernameLabel, username);
+    private HBox hBox2 = new HBox(passwordLabel, password);
+    private VBox vBox2 = new VBox(hBox1, hBox2, loginButton);
+    private VBox vBox3 = new VBox(helpButton);
+    private BorderPane centralBorderPane = new BorderPane();
 
     public static synchronized LoginView getInstance(){
         if (loginView_instance == null){
             loginView_instance = new LoginView();
         }
         return loginView_instance;
+    }
+
+    public Label getWelcomeLabel2() {
+        return welcomeLabel2;
+    }
+
+    public void setWelcomeLabel2(Label welcomeLabel2) {
+        this.welcomeLabel2 = welcomeLabel2;
     }
 
     public TextField getUsername() {
@@ -51,22 +69,41 @@ public class LoginView extends Stage {
     }
 
     private LoginView(){
-        bp.setTop(topLabel);
-        bp.setCenter(vBox1);
-        bp.setBottom(loginButton);
-        BorderPane.setAlignment(topLabel, Pos.CENTER);
-        BorderPane.setAlignment(vBox1, Pos.CENTER);
-        BorderPane.setAlignment(loginButton, Pos.CENTER);
         vBox1.setAlignment(Pos.CENTER);
+        vBox1.setSpacing(5);
+        vBox1.setPadding(new Insets(0, 0, 10, 0));
+        welcomeLabel2.setAlignment(Pos.CENTER);
+        welcomeLabel1.setFont(new Font(20));
+
+        hBox1.setAlignment(Pos.CENTER);
+        hBox1.setSpacing(25);
+        hBox2.setAlignment(Pos.CENTER);
+        hBox2.setSpacing(28); // Lazy fix
+        vBox2.setSpacing(10);
+        vBox2.setAlignment(Pos.CENTER);
+
+        vBox3.setAlignment(Pos.BOTTOM_RIGHT);
+        helpButton.setUnderline(true);
+        vBox3.setPadding(new Insets(5));
+
+        centralBorderPane.setTop(vBox1);
+        centralBorderPane.setCenter(vBox2);
+        centralBorderPane.setBottom(vBox3);
+
+
         username.setMaxWidth(150);
         username.setTooltip(new Tooltip("Enter your username"));
         password.setMaxWidth(150);
         password.setTooltip(new Tooltip("Enter your password"));
-        vBox1.setSpacing(15);
-        this.setScene(new Scene(bp, 960, 540));
+        this.setScene(new Scene(centralBorderPane, 360, 360));
         this.show();
         LoginController loginController = new LoginController(this);
+
         loginButton.setId("loginButton");
         loginButton.setOnAction(loginController);
+        username.setId("loginField");
+        password.setId("loginField");
+        username.setOnAction(loginController);
+        password.setOnAction(loginController);
     }
 }
